9bdc97473834e9abad8303478db0c3da975b4deb
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-02-03 19:25:07 +0900

    Initial Commit

---
+++ SegueViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueViewController
+//
+//  Created by hiraoka on 2020/02/03.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
